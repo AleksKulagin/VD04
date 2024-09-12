3164efdee0ea80bc411c4deb26b8c58155eeeb56
AleksKulagin <master-tech@yandex.ru>
2024-09-12 18:00:43 +1000

    1

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return f"<h1>Текущая дата и время:</h1><p>{current_time}</p>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
